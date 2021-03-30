072a64907c942cd7f5459a1a2f7f7467c27fb88c
laban405 <laban405@gmail.com>
2021-03-30 19:56:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import SideBar from "./components/SideBar";
+import Main from "./components/Main";
 
 function App() {
+  const [activeItem, setActiveItem] = useState("");
+  const [mode, setMode] = useState("appmode");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="topbar">
+        <Main setMode={setMode} setActiveItem={setActiveItem}/>
+      </div>
+      <SideBar
+        activeItem={activeItem}
+        setActiveItem={setActiveItem}
+        mode={mode}
+      />
     </div>
   );
 }
---
+++ src/components/Logo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Logo.css";
+
+const Logo = () => {
+  return <div className="logo">Logo</div>;
+};
+
+export default Logo;
